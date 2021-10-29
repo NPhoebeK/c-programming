53c2e2641706897b4ea32cc4b807dcfa374909e5
NphoebeK <91840080+NPhoebeK@users.noreply.github.com>
2021-10-29 10:14:01 +0300

    Delete variable_declaration[1].c

--- variable_declaration[1].c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-void main ()
-{
-//declare three variables
-int first number,second number,total sum;
-//please input the value of the first number to add
-printf("enter first number");
-//store the first number using scan
-scanf("%d",&first number);
