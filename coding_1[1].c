ea062ef882d86beea9922a38f40504dc0a4c80d7
NphoebeK <91840080+NPhoebeK@users.noreply.github.com>
2021-10-29 10:15:21 +0300

    Add files via upload

---
+++ coding_1[1].c
@@ -0,0 +1,13 @@
+/*Arithmetic operators division*\
+#include <stdio.h>
+int main()
+{
+int a, b,divide;
+printf("\n enter value to a:");
+scanf("%d, $a");
+printf("\n enter value to b:");
+scanf("%d, $b");
+divide=a÷b
+printf("\n divide=%d",divide);
+return 0;
+}
